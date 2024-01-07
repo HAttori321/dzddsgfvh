eb576f587552421f05ce082756db92ce9cbf6a31
hattori <103142867+HAttori321@users.noreply.github.com>
2024-01-07 13:44:46 +0200

    dgfdfghgfjdse wvtgr.py

---
+++ dgfdfghgfjdse wvtgr.py
@@ -0,0 +1,53 @@
+# def text():
+#     text=(
+#         "Don't compare yourself with anyone in this world…\n"
+#         "if you do so, you are insulting yourself.\n"
+#         "Bill Gates")
+#     print(text)
+# text()
+2
+# def parniNumber(start,end):
+#     for num in range(start,end+1):
+#         if num%2==0:
+#             print(num)
+# start=5
+# end=15
+# parniNumber(start,end)
+3
+# def printSquare(length,symbol,filled):
+#     for _ in range(length):
+#         if filled:
+#             print(symbol*length)
+#         else:
+#             if _ == 0 or _==length-1:
+#                 print(symbol*length)
+#             else:
+#                 print(symbol+' '*(length-2)+symbol)
+# length=8
+# symbol='[]'
+# square2=True
+# printSquare(length,symbol,square2)
+4
+# def minimum(num1, num2, num3, num4):
+#     minimum=min(num1, num2, num3, num4)
+#     return minimum
+# result=minimum(7,2,4,21)
+# print("Minimum:",result)
+5
+# def dobytok(meja1,meja2):
+#     if meja1>meja2:
+#         meja1,meja2=meja2,meja1
+#     dobytok=1
+#     for num in range(meja1,meja2 + 1):
+#         dobytok*=num
+
+#     return dobytok
+# result=dobytok(10,20)
+# print("Добуток чисел:",result)
+6
+# def count(number):
+#     count=len(str(number))
+#     return count
+# number1=1234
+# result=count(number1)
+# print("Кількість цифр:", result)
